ec67916f27f868703d7a94db534eed07048aa7a0
Zino Chan <devzinochan@gmail.com>
2021-07-22 14:58:47 +0100

    initial init add base structure add home page initial components + images

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '@fontsource/open-sans';
+import '@fontsource/source-sans-pro';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
